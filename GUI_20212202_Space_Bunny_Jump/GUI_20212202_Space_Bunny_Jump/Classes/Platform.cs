469fca531a002f66bbb74dcd4c578bc6fab6cd38
RBandesz <rabakoziandras@gmail.com>
2022-03-28 12:35:34 +0200

    Create solution

---
+++ GUI_20212202_Space_Bunny_Jump/GUI_20212202_Space_Bunny_Jump/Classes/Platform.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUI_20212202_Space_Bunny_Jump.Classes
+{
+    class Platform
+    {
+    }
+}
